ed79a3554243e9d9094bc25132712577795eb5e4
liangpengyv <mrliangpengyv@163.com>
2017-07-06 19:03:41 +0800

    First commit

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputLabel: UILabel!
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var inputText: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func input0(_ sender: UIButton) {
+        inputText += "0"
+        inputLabel.text = inputText
+    }
+    @IBAction func input1(_ sender: UIButton) {
+    }
+    @IBAction func input2(_ sender: UIButton) {
+    }
+    @IBAction func input3(_ sender: UIButton) {
+    }
+    @IBAction func input4(_ sender: UIButton) {
+    }
+    @IBAction func input5(_ sender: UIButton) {
+    }
+    @IBAction func input6(_ sender: UIButton) {
+    }
+    @IBAction func input7(_ sender: UIButton) {
+    }
+    @IBAction func input8(_ sender: UIButton) {
+    }
+    @IBAction func input9(_ sender: UIButton) {
+    }
+    @IBAction func inputPoint(_ sender: UIButton) {
+    }
+    @IBAction func inputAdd(_ sender: UIButton) {
+    }
+    @IBAction func inputSub(_ sender: UIButton) {
+    }
+    @IBAction func inputMul(_ sender: UIButton) {
+    }
+    @IBAction func inputDiv(_ sender: UIButton) {
+    }
+    @IBAction func inputEqual(_ sender: UIButton) {
+    }
+    @IBAction func inputClear(_ sender: UIButton) {
+    }
+    
 
+    
 }
 
